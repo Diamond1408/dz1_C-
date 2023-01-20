d98518a8f630c86b8ac67401b7ad542a46481dec
Diamond <fkvfptq1408@gmail.com>
2023-01-20 13:40:44 +0300

    DZ1_C#

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+System.Console.WriteLine("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+int min = number2;
+    if (number2 > max)
+    {
+        max = number2;
+        min = number1;
+    }
+System.Console.Write("max = ");    
+System.Console.WriteLine(max);
+System.Console.Write("min = ");
+System.Console.WriteLine(min);
---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+System.Console.WriteLine("Введите любое число начиная от 1:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+    while ( count <= number)
+    {
+       System.Console.WriteLine(count);
+       count = count + 2;
+    }
